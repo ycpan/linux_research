3c4551b0e1612cce1d4289e97029a9a5bd450731
pan yongcan <panyongcan@incostar.com>
2022-05-09 10:46:03 +0800

    Static site deploy

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+######!/bin/bash
+set -e
+#nvm use 10
+echo "生成SUMMARY文件"
+(gitbook serve) &
+sleep 20
+echo "提交变动"
+git add .
+git commit -m "update"
+git push
+echo "发布"
+GIT_REPO_URL=$(git config --get remote.origin.url)
+#sed -i 's/if(m)for(n.handler&&/if(m)for(n.handler&&/g' gitbook/theme.js
+cd _book
+sed -i '' 's/if(m)for(n.handler/if(false)for(n.handler/g' gitbook/theme.js # -i 要加空格，否则报错。感觉和liunx中的不太一样
+mkdir .deploy
+cp -R ./* .deploy
+cd .deploy
+git init .
+git remote add github $GIT_REPO_URL
+git checkout -b gh-pages
+git add .
+git commit -am "Static site deploy"
+git push github gh-pages --force
+cd ..
+rm -rf .deploy
